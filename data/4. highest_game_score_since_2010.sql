3999d3e59bd1badfbdb02a856cc1ac4011b65a03
Muhammad Yasin Fajari <121071732+myasinfajar@users.noreply.github.com>
2022-12-20 10:10:18 +0700

    Create 4. highest_game_score_since_2010.sql

---
+++ data/4. highest_game_score_since_2010.sql
@@ -0,0 +1,20 @@
+# Highest Game Score since 2010
+
+SELECT
+  scheduled_date,
+  name home_team,
+  alias home_team_alias,
+  points_game home_team_points,
+  opp_name,
+  opp_alias,
+  opp_points_game,
+  (points_game + opp_points_game) total_points
+FROM
+  `bigquery-public-data.ncaa_basketball.mbb_teams_games_sr`
+WHERE
+  season > 2010
+ORDER BY
+  total_points
+  DESC
+LIMIT
+  5
