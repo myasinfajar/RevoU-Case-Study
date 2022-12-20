8ae93b000e3453da0b9023797349efe7b1783f3f
Muhammad Yasin Fajari <121071732+myasinfajar@users.noreply.github.com>
2022-12-20 09:54:29 +0700

    Update typesof_basketball_events.sql

--- typesof_basketball_events.sql
+++ typesof_basketball_events.sql
@@ -1,11 +1,11 @@
 # Types and total of Basketball play events occur
 
 SELECT
-  event_type,
-  COUNT(*)
+  event_type, # take **event_type** values
+  COUNT(*) # count all the values
 FROM
   `bigquery-public-data.ncaa_basketball.mbb_pbp_sr`
 GROUP BY
-  1
+  1 # means event_type
 ORDER BY
-  2 DESC
+  2 DESC # means count(*) ordered descending
